32bd615a0a35f1fa33ac30d9db81b1cf9bba68ad
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2024-01-22 12:50:19 +0530

    minor update

--- src/main/java/com/project/taskmanagement/controller/ReportController.java
+++ src/main/java/com/project/taskmanagement/controller/ReportController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.taskmanagement.exception.BusinessException;
 import com.project.taskmanagement.service.ReportService;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/api/report")
 public class ReportController {
