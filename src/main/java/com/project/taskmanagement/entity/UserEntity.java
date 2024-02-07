57e257a9fc2831ba7864f1ef7dba578b0e5cd20e
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2024-02-07 14:13:48 +0530

    minor update

--- src/main/java/com/project/taskmanagement/entity/UserEntity.java
+++ src/main/java/com/project/taskmanagement/entity/UserEntity.java
@@ -14,9 +14,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
 @Setter
 @Getter
 @Entity
